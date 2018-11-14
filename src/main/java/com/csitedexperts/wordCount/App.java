2ed5be450ccb6e55c18a396b1f8206b36ab220c2
CSITEd Experts <csitedexperts@gmail.com>
2018-11-14 13:34:07 -0500

    dependencies added

---
+++ src/main/java/com/csitedexperts/wordCount/App.java
@@ -0,0 +1,13 @@
+package com.csitedexperts.wordCount;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
